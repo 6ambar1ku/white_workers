a2b2920fc645cbade1db3a258bc11a9764cc3523
dandanbatake <kosei.maruyama@tech.nflabs.jp>
2021-09-15 19:27:41 +0900

    スコアをソートして表示

--- csv_read.py
+++ csv_read.py
@@ -1,28 +1,29 @@
 import csv
-#import pyimpfuzzy
 import ssdeep
 import sys
-#import pefile
 
 csv_file = open("../malware.csv", "r", encoding="ms932", errors="", newline="" )
 #リスト形式
 f = csv.reader(csv_file, delimiter=",", doublequote=True, lineterminator="\r\n", quotechar='"', skipinitialspace=True)
-#辞書形式
-# f = csv.DictReader(csv_file, delimiter=",", doublequote=True, lineterminator="\r\n", quotechar='"', skipinitialspace=True)
 
 fuzzyhash1 = "98304:QqPoBhz1aRxcSUDk36SAEdhvxWa9P593R8yAVp2g3x:QqPe1Cxcxk3ZAEUadzR8yc4gB"
 
 header = next(f)
-# print(header)
 
-i = 1
+dic = {}
+pc = 0
 for row in f:
+    if pc > 100:
+        break
+    pc += 1
     fuzzyhash2 = row[4]
-
     compare_value = ssdeep.compare(fuzzyhash1,fuzzyhash2)
-    if compare_value > 98:
-        print(fuzzyhash2)
-        print(">>> ssdeep.compare score: %i/100¥n" % compare_value)
+    dic[row[3]] = compare_value
+
+dic_sorted = sorted(dic.items(), key=lambda x:x[1], reverse = True)
+for i in range(10):
+    print(dic_sorted[i])
+    print("------")
 
 # 閲覧用
 # i = 1
