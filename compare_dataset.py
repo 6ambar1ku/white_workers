f3d25b60059890bd4722d026c4424fe987caa6ad
dandanbatake <kosei.maruyama@tech.nflabs.jp>
2021-09-15 19:42:45 +0900

    ソート機能を関数にした

--- compare_dataset.py
+++ compare_dataset.py
@@ -1,37 +1,45 @@
+
 import csv
 import ssdeep
 import sys
 
-csv_file = open("../malware.csv", "r", encoding="ms932", errors="", newline="" )
-#リスト形式
-f = csv.reader(csv_file, delimiter=",", doublequote=True, lineterminator="\r\n", quotechar='"', skipinitialspace=True)
+def comp(fuzzyhash1):
 
-fuzzyhash1 = "98304:QqPoBhz1aRxcSUDk36SAEdhvxWa9P593R8yAVp2g3x:QqPe1Cxcxk3ZAEUadzR8yc4gB"
+    csv_file = open("../malware.csv", "r", encoding="ms932", errors="", newline="" )
+    #リスト形式
+    f = csv.reader(csv_file, delimiter=",", doublequote=True, lineterminator="\r\n", quotechar='"', skipinitialspace=True)
 
-header = next(f)
+    #fuzzyhash1 = "98304:QqPoBhz1aRxcSUDk36SAEdhvxWa9P593R8yAVp2g3x:QqPe1Cxcxk3ZAEUadzR8yc4gB"
 
-dic = {}
-pc = 0
-for row in f:
-    if pc > 100:
-        break
-    pc += 1
-    fuzzyhash2 = row[4]
-    compare_value = ssdeep.compare(fuzzyhash1,fuzzyhash2)
-    dic[row[3]] = compare_value
+    header = next(f)
 
-dic_sorted = sorted(dic.items(), key=lambda x:x[1], reverse = True)
-for i in range(10):
-    print(dic_sorted[i])
-    print("------")
+    dic = {}
+    pc = 0
+    for row in f:
+        if pc > 100:
+            break
+        pc += 1
+        fuzzyhash2 = row[4]
+        compare_value = ssdeep.compare(fuzzyhash1,fuzzyhash2)
+        dic[row[3]] = compare_value
 
-# 閲覧用
-# i = 1
-# for row in f:
-#     i+=1
-#     # if i==10:
-#     #     break
-#     fuzzyhash2 = row
-#     #print(fuzzyhash2)
-# print(i)
+    dic_sorted = sorted(dic.items(), key=lambda x:x[1], reverse = True)
+    
+    return_dic = {}
+    for i in range(10):
+        print(dic_sorted[i])
+        print("------")
+        return_dic[dic_sorted[i][0]] = dic_sorted[i][1] 
+
+    return return_dic
+    
+    # 閲覧用
+    # i = 1
+    # for row in f:
+    #     i+=1
+    #     # if i==10:
+    #     #     break
+    #     fuzzyhash2 = row
+    #     #print(fuzzyhash2)
+    # print(i)
 
