ef513fdf85cb021bf86a5159e22f6868789d62e5
t_kamiyama <takahiro.kamiyama@tech.nflabs.jp>
2021-09-15 18:48:46 +0900

    とりあえず動いた

--- csv_read.py
+++ csv_read.py
@@ -4,26 +4,43 @@ import ssdeep
 import sys
 #import pefile
 
-csv_file = open("./malware.csv", "r", encoding="ms932", errors="", newline="" )
+csv_file = open("../malware.csv", "r", encoding="ms932", errors="", newline="" )
 #リスト形式
 f = csv.reader(csv_file, delimiter=",", doublequote=True, lineterminator="\r\n", quotechar='"', skipinitialspace=True)
 #辞書形式
 # f = csv.DictReader(csv_file, delimiter=",", doublequote=True, lineterminator="\r\n", quotechar='"', skipinitialspace=True)
 
-#header = next(f)
-#print(header)
-#i = 1
-# for row in f:
-#     i+=1
-#     if i==10:
-#         break
-#     #rowはList
-#     #row[0]で必要な項目を取得することができる
-#     print(row)
-
+# fuzzyhash1 = "98304:QqPoBhz1aRxcSUDk36SAEdhvxWa9P593R8yAVp2g3x:QqPe1Cxcxk3ZAEUadzR8yc4gB"
+# fuzzyhash2 = "98304:QqPoBhz1aRxcSUDk36SAEdhvxWa9P593R8yAVp2g3x:QqPe1Cxcxk3ZAEUadzR8yc4gB"
+# print("ssdeep1: %s" % fuzzyhash1)
+# print("ssdeep2: %s" % fuzzyhash2)
+# print(">>> ssdeep.compare score: %i/100¥n" % ssdeep.compare(fuzzyhash1,fuzzyhash2))
 
 fuzzyhash1 = "98304:QqPoBhz1aRxcSUDk36SAEdhvxWa9P593R8yAVp2g3x:QqPe1Cxcxk3ZAEUadzR8yc4gB"
-fuzzyhash2 = "98304:QqPoBhz1aRxcSUDk36SAEdhvxWa9P593R8yAVp2g3x:QqPe1Cxcxk3ZAEUadzR8yc4gB"
-print("ssdeep1: %s" % fuzzyhash1)
-print("ssdeep2: %s" % fuzzyhash2)
-print(">>> ssdeep.compare score: %i/100¥n" % ssdeep.compare(fuzzyhash1,fuzzyhash2))
+
+header = next(f)
+# print(header)
+
+i = 1
+for row in f:
+    # i+=1
+    # if i==10:
+    #     break
+    fuzzyhash2 = row[4]
+    #rowはList
+    #row[0]で必要な項目を取得することができる
+    compare_value = ssdeep.compare(fuzzyhash1,fuzzyhash2)
+    if compare_value > 98:
+        print(fuzzyhash2)
+        print(">>> ssdeep.compare score: %i/100¥n" % compare_value)
+
+# 閲覧用
+i = 1
+for row in f:
+    i+=1
+    # if i==10:
+    #     break
+    fuzzyhash2 = row
+    #print(fuzzyhash2)
+print(i)
+
