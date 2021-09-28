56375c5eb195260cd36eefdf1125c81730811f05
t_kamiyama <takahiro.kamiyama@tech.nflabs.jp>
2021-09-28 19:00:29 +0900

    リファクタリング

--- compare_dataset.py
+++ compare_dataset.py
@@ -1,16 +1,12 @@
-
 import csv
 import ssdeep
 import sys
 
-def comp(fuzzyhash1):
+def comp(ssdeep1):
 
     csv_file = open("../malware.csv", "r", encoding="ms932", errors="", newline="" )
-    #リスト形式
+    # リスト形式で読み込む
     f = csv.reader(csv_file, delimiter=",", doublequote=True, lineterminator="\r\n", quotechar='"', skipinitialspace=True)
-
-    #fuzzyhash1 = "98304:QqPoBhz1aRxcSUDk36SAEdhvxWa9P593R8yAVp2g3x:QqPe1Cxcxk3ZAEUadzR8yc4gB"
-
     header = next(f)
 
     dic = {}
@@ -19,27 +15,18 @@ def comp(fuzzyhash1):
         if pc > 100:
             break
         pc += 1
-        fuzzyhash2 = row[4]
-        compare_value = ssdeep.compare(fuzzyhash1,fuzzyhash2)
+        ssdeep2 = row[4]
+        compare_value = ssdeep.compare(ssdeep1,ssdeep2)
         dic[row[3]] = compare_value
 
     dic_sorted = sorted(dic.items(), key=lambda x:x[1], reverse = True)
     
     return_dic = {}
+    # 上位10件を出力する
     for i in range(10):
         print(dic_sorted[i])
         print("------")
         return_dic[dic_sorted[i][0]] = dic_sorted[i][1] 
 
     return return_dic
-    
-    # 閲覧用
-    # i = 1
-    # for row in f:
-    #     i+=1
-    #     # if i==10:
-    #     #     break
-    #     fuzzyhash2 = row
-    #     #print(fuzzyhash2)
-    # print(i)
 
---
+++ main.py
@@ -0,0 +1,5 @@
+import compare_dataset as compd
+
+ssdeep = "98304:QqPoBhz1aRxcSUDk36SAEdhvxWa9P593R8yAVp2g3x:QqPe1Cxcxk3ZAEUadzR8yc4gB"
+
+compd.comp(ssdeep)
