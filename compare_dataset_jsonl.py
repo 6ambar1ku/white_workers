31b2aba15298d3e9c3d5749498db8946429b66d5
bambariku <r.bamba@tech.nflabs.jp>
2021-10-05 18:37:24 +0900

    repair path

--- compare_dataset_jsonl.py
+++ compare_dataset_jsonl.py
@@ -6,7 +6,7 @@ def comp(ssdeep1):
     # ファイルから1行ずつ読み込む
     dic = {}
 
-    with open("Desktop/white_workers/MWScup_FFRI Datasets/ffridataset2021_malware.jsonl", "r") as f:
+    with open("../MWScup_FFRI Datasets/ffridataset2021_malware.jsonl", "r") as f:
         row = f.readline() #1行目を読み込む
         i = 0
         pbar = tqdm(total=75000)
